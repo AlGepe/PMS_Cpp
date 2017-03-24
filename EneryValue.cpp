940732786f0b9908deac961d648b27986c1e4bf0
AlGepe <alvarodiezgp@gmail.com>
2017-03-24 09:01:51 +0100

    Forgotten to add new class

---
+++ EneryValue.cpp
@@ -0,0 +1,34 @@
+#include <EneryValue.h>
+
+EneryValue::EneryValue(double u, double k) :
+	_u(u),
+	_k(k)
+{
+}
+
+/*
+ * Setters
+ */
+void EneryValue::u(double newU)
+{
+	_u = newU;
+}
+
+void EneryValue::k(double newK)
+{
+	_k = newK;
+}
+
+/*
+ * Getters
+ */
+double EneryValue::u()
+{
+	return _u;
+}
+
+double EneryValue::k()
+{
+	return _k;
+}
+
